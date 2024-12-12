aaeec39e450d8772a0efa88ffb09dbe669db0a3c
Razim Rafi <8910razim@gmail.com>
2024-12-12 17:10:08 +0530

    new change

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -27,7 +27,7 @@ function Login() {
 
   const backendURL = String(import.meta.env.VITE_BACKEND_URL)
   const production = Number(import.meta.env.VITE_PRODUCTION)
-
+  console.log(production)
   const api = production == 1 ? backendURL : 'http://localhost:3001'
   console.log(api)
   const onsubmit = async (data) => {
